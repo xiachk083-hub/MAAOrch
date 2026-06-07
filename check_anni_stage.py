07e982d0e82ad5013c8ec90399292d3b5e34b1e0
user <user@maaorch.local>
2026-06-08 00:24:05 +0800

    cleanup

--- check_anni_stage.py
+++
@@ -1,8 +0,0 @@
-import json
-for inst in ['maa/instances/1', 'maa/instances/2']:
-    fn = f'{inst}/config/gui.new.json'
-    d = json.load(open(fn, 'rb'))
-    tq = d.get('Configurations',{}).get('Default',{}).get('TaskQueue',[])
-    for item in tq:
-        if item.get('TaskType') == 'Fight':
-            print(f'{inst}: AnnihilationStage={item.get("AnnihilationStage")!r}')
